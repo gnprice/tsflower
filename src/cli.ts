6f72d449e4025e7407d3b23dafca58a204bb23af
Greg Price <greg@zulip.com>
2022-04-26 15:27:09 -0700

    cli: Further error-handling in parsing command line

--- src/cli.ts
+++ src/cli.ts
@@ -1,41 +1,78 @@
 /**
  * CLI for TsFlower.
  *
- * For CLI usage, see `printUsage` below, or run:
+ * For CLI usage, see `getUsage` below, or run:
  *
  *     $ tsflower --help
  */
 import process from "process";
 import { convertFileToString } from "./index";
 
+class CliError extends Error {
+  readonly messages: string[];
+  readonly exitCode: number;
+
+  constructor(messages: string[], exitCode?: number) {
+    super(messages[0]);
+    this.messages = messages;
+    this.exitCode = exitCode ?? 1;
+  }
+}
+
+interface CliCommand {
+  readonly inputFilename: string;
+}
+
 main();
 
 function main() {
   const argv = process.argv.slice(2);
 
+  const command = parseCommandLineOrExit(argv);
+
+  process.stdout.write(convertFileToString(command.inputFilename));
+}
+
+function parseCommandLineOrExit(argv: string[]): CliCommand {
+  try {
+    return parseCommandLine(argv);
+  } catch (e) {
+    if (e instanceof CliError) {
+      for (const message of e.messages) {
+        process.stderr.write(`tsflower: ${message}\n`);
+      }
+      process.stderr.write(getUsage());
+      process.exit(e.exitCode);
+    }
+
+    throw e;
+  }
+}
+
+function parseCommandLine(argv: string[]): CliCommand {
   if (argv.length !== 1) {
-    usageError();
+    usageError("require 1 argument");
   }
 
   if (argv[0] === "--help") {
-    printUsage();
-    process.exit(0);
+    throw new CliError([], 0);
   }
 
   const [inputFilename] = argv;
 
-  process.stdout.write(convertFileToString(inputFilename));
+  return {
+    inputFilename,
+  };
 }
 
-function usageError(): never {
-  printUsage();
-  process.exit(1);
+function usageError(message: string): never {
+  throw new CliError([message]);
 }
 
-function printUsage() {
-  process.stderr.write(`\
+function getUsage() {
+  return `\
 Usage: tsflower path/to/some/file.d.ts
 
 Prints result to stdout.
-`);
+`;
 }
