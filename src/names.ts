512756018ae405171245c93e7da0d77549e30138
Greg Price <greg@zulip.com>
2022-05-09 16:34:08 -0700

    names [nfc]: Factor out escapeAsIdContinue

--- src/names.ts
+++ src/names.ts
@@ -17,6 +17,23 @@ export function escapeNameAsIdentifierWithPrefix(
   // In particular, after the first character, each character can be `$`
   // or any character with the `ID_Continue` Unicode property, among others.
 
+  // For our construction, first, we produce a string where all characters
+  // have `ID_Continue`, as an invertible function of `name`.
+  const escapedName = escapeAsIdContinue(name);
+
+  // Then we delimit the prefix from the escaped name with `$`, which lacks
+  // `ID_Continue` and therefore cannot appear in the escaped name.
+  return prefix + "$" + escapedName;
+}
+
+/**
+ * A string of all ID_Continue, as a deterministic, invertible function.
+ *
+ * That is, each character in the result has the `ID_Continue` Unicode
+ * property; and the result is always the same when called with the same
+ * argument, and always different when called with different arguments.
+ */
+function escapeAsIdContinue(name: string): string {
   // For our construction:
   //  * Characters in `name` with `ID_Continue`, other than `_`, we use
   //    verbatim.
@@ -27,6 +44,7 @@ export function escapeNameAsIdentifierWithPrefix(
     /\P{ID_Continue}|_/gu,
     (c) => `_${c.codePointAt(0)!.toString(16)}_`,
   );
+
   // This escaping always gives different results for different inputs,
   // because the following code would reconstruct the input:
   //   assert(
@@ -36,7 +54,5 @@ export function escapeNameAsIdentifierWithPrefix(
   //       ),
   //   );
 
-  // Then we delimit the prefix from the escaped name with `$`, which lacks
-  // `ID_Continue` and therefore cannot appear in the escaped name.
-  return prefix + "$" + escapedName;
+  return escapedName;
 }
