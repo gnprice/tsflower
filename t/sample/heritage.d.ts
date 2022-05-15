1f498f33f048268749d432e2fe0cd947993dc551
Greg Price <greg@zulip.com>
2022-05-15 11:32:14 -0700

    test: Expand tests of class-extends and interface-extends

--- t/sample/heritage.d.ts
+++ t/sample/heritage.d.ts
@@ -6,5 +6,20 @@
  * with our rewriting (of references with external translation).
  */
 
-export declare class C<T> {}
-export declare class D extends C<string> {}
+// Class extends.
+
+export declare class C {}
+export declare class C1<T = number> {}
+
+export declare class D1 extends C1<string> {}
+
+export declare class D0 extends C1 {} // should become C1<>
+export declare class D extends C {} // … while this stays just C
+
+// Interface extends.
+
+export declare interface I {}
+export declare interface I1<T = number> {}
+
+export declare interface J1 extends I1<string> {}
+export declare interface J extends I, I1 {} // should become I, I1<>
