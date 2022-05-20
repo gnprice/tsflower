773cb204f5f6ecdd411a94ad2f31b6db0c954fcb
Greg Price <greg@zulip.com>
2022-05-19 22:33:48 -0700

    test [nfc]: Reorder sample/react tests to match subst/react

--- t/sample/react.d.ts
+++ t/sample/react.d.ts
@@ -6,6 +6,51 @@ import {
 import React2 from 'react'; // TODO make this fully work too; see comment at convertTypeArguments
 import * as React from 'react';
 
+//
+// Keep this file in the same order as subst/react.js.flow
+// (which in turn is the same order as `@types/react/index.d.ts`.)
+//
+
+var refObject: React.RefObject<number>;
+
+// TODO(test): Add Flow test code to confirm this interoperates with flowlib
+var ref: React.Ref<number>;
+var ref2: React.Ref<React.Component<>>;
+
+// TODO(test): Add Flow test code to confirm this interoperates with flowlib
+// TODO(test): Test RefAttributes in separate file from Ref, to exercise
+//   dependencies
+var refAttributes: React.RefAttributes<React.Component<>>;
+
+// `React.ReactElement` corresponds to `React.Element` in Flow,
+// and with different type arguments.
+var reactElement: {
+  // a1: ReactElement;
+  a2: React.ReactElement;
+  a3: React2.ReactElement;
+  // b1: ReactElement<AProps>;
+  b2: React.ReactElement<AProps>;
+  b3: React2.ReactElement<AProps>;
+  // c1: ReactElement<AProps, "div">;
+  c2: React.ReactElement<AProps, 'div'>;
+  c3: React2.ReactElement<AProps, 'div'>;
+};
+
+var reactNode: React.ReactNode;
+
+var context1: {
+  providerProps: React.ProviderProps<string>;
+  consumerProps: React.ConsumerProps<string>;
+};
+
+var namedExoticComponent: React.NamedExoticComponent<{}>;
+
+var context2: {
+  provider: React.Provider<string>;
+  consumer: React.Consumer<string>;
+  context: React.Context<string>;
+};
+
 // `React.Component` in Flow requires its first argument.
 // Test that we rewrite references that lack that.
 // First in TypeReference form…
@@ -28,70 +73,31 @@ declare class A extends Component<AProps> {}
 declare class AA extends React.Component<AProps> {}
 declare class AAA extends React2.Component<AProps> {}
 
-// `React.ReactElement` corresponds to `React.Element` in Flow,
-// and with different type arguments.
-var reactElement: {
-  // a1: ReactElement;
-  a2: React.ReactElement;
-  a3: React2.ReactElement;
-  // b1: ReactElement<AProps>;
-  b2: React.ReactElement<AProps>;
-  b3: React2.ReactElement<AProps>;
-  // c1: ReactElement<AProps, "div">;
-  c2: React.ReactElement<AProps, 'div'>;
-  c3: React2.ReactElement<AProps, 'div'>;
-};
+var functionComponent0: React.FunctionComponent;
+var functionComponent1: React.FunctionComponent<{ x: number }>;
+
+// TODO(test): Add Flow test code to confirm this interoperates with flowlib
+var forwardRefExoticComponent: React.ForwardRefExoticComponent<
+  { x: string } & React.RefAttributes<React.Component<>>
+>;
+
+var propsWithoutRef1: React.PropsWithoutRef<{ x: number; ref: string }>;
+var propsWithoutRef2: React.PropsWithoutRef<{ x: number }>;
+var propsWithoutRef3: React.PropsWithoutRef<
+  { x: number; ref: string } | { y: number }
+>;
+
+var propsWithChildren: React.PropsWithChildren<{ x: number }>;
 
 var componentProps: {
   // TODO(test): Add Flow test code to confirm this interoperates
   a: React.ComponentProps<A>;
 };
 
-var propsManipulation: {
-  propsWithChildren: React.PropsWithChildren<{ x: number }>;
-  propsWithoutRef1: React.PropsWithoutRef<{ x: number; ref: string }>;
-  propsWithoutRef2: React.PropsWithoutRef<{ x: number }>;
-  propsWithoutRef3: React.PropsWithoutRef<
-    { x: number; ref: string } | { y: number }
-  >;
-};
+// TODO(test): Add Flow test code to check this against React.memo etc.
+var memoExoticComponent: React.MemoExoticComponent<A>;
 
-var miscComponents: {
-  functional0: React.FunctionComponent;
-  functional1: React.FunctionComponent<{ x: number }>;
-  // TODO(test): Add Flow test code to check this against React.memo etc.
-  memo: React.MemoExoticComponent<A>;
-  named: React.NamedExoticComponent<{}>;
-};
-
-var reactNode: React.ReactNode;
-
-var ref: {
-  mutableRefObject: React.MutableRefObject<number>;
-  refObject: React.RefObject<number>;
-
-  // TODO(test): Add Flow test code to confirm this interoperates with flowlib
-  ref: React.Ref<number>;
-  ref2: React.Ref<React.Component<>>;
-
-  // TODO(test): Add Flow test code to confirm this interoperates with flowlib
-  // TODO(test): Test RefAttributes in separate file from Ref, to exercise
-  //   dependencies
-  refAttributes: React.RefAttributes<React.Component<>>;
-
-  // TODO(test): Add Flow test code to confirm this interoperates with flowlib
-  forwardRefExoticComponent: React.ForwardRefExoticComponent<
-    { x: string } & React.RefAttributes<React.Component<>>
-  >;
-};
-
-var context: {
-  providerProps: React.ProviderProps<string>;
-  consumerProps: React.ConsumerProps<string>;
-  provider: React.Provider<string>;
-  consumer: React.Consumer<string>;
-  context: React.Context<string>;
-};
+var mutableRefObject: React.MutableRefObject<number>;
 
 var jsx: {
   element: JSX.Element;
