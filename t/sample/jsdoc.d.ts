9a8b292de3b6e7fa75251d052dd0afa4827628cb
Greg Price <greg@zulip.com>
2022-06-04 16:18:45 -0700

    jsdoc test more explicit

--- t/sample/jsdoc.d.ts
+++ t/sample/jsdoc.d.ts
@@ -1,22 +1,22 @@
-/** Toplevel. */
+/** Toplevel jsdoc (keep) */
 
 /* eslint-disable  */
-/*! Pinned comment. */   /** Here's some jsdoc. */
+/*! Pinned comment (keep) */   /** Here's some jsdoc (keep) */
 
-/* Block implementation comment. */
-export declare type T = number; /** more on T */
+/* Block implementation comment (DROP) */
+export declare type T = number; /** more on T (keep) */
 
-export type U = /** a TODO missing because "trailing" */ 'a' | 'b' /** b */ | 'c';
+export type U = /** a TODO missing because "trailing" the `=` */ 'a' | 'b' /** b (keep) */ | 'c';
 export type U1 =
- /** a */ 'a' | 'b' /** b */ | 'c';
+ /** a (keep) */ 'a' | 'b' /** b (keep) */ | 'c';
 export type U2 =
- | /** a TODO missing because "trailing" */ 'a' | 'b' /** b */ | 'c';
+ | /** a TODO missing because "trailing" the `|` */ 'a' | 'b' /** b (keep) */ | 'c';
 export type U3 =
  | /** a TODO missing
-  because "trailing" */ 'a' | 'b' /** b */ | 'c';
+  because still "trailing" */ 'a' | 'b' /** b (keep) */ | 'c';
 
 export type LongLengthyProfuselyVerboselyNamedTypeCalledT<
-  /** JsDoc on type parameter S */
+  /** JsDoc on type parameter S (keep) */
   S
 > = {
   x: {
@@ -25,6 +25,7 @@ export type LongLengthyProfuselyVerboselyNamedTypeCalledT<
        * z of the y.
        *
        * Details about z
+       * (keep)
        */
       z: S;
     };
@@ -32,11 +33,11 @@ export type LongLengthyProfuselyVerboselyNamedTypeCalledT<
 };
 
 export type S = LongLengthyProfuselyVerboselyNamedTypeCalledT<{
-  /** Some jsdoc on this property */
+  /** Some jsdoc on this property (keep) */
   a1: LongLengthyProfuselyVerboselyNamedTypeCalledT<
       { 
         /**
-         * Some jsdoc on this inner property.  How does it indent?
+         * Some jsdoc on this inner property.  How does it indent? (keep)
          */
         x: number;
         y: void;
@@ -44,16 +45,18 @@ export type S = LongLengthyProfuselyVerboselyNamedTypeCalledT<{
     >;
 }>;
 
-/** Stray jsdoc */
-// Stray comment
+/** Stray jsdoc (keep) */
+// Stray comment (DROP)
 /**
  * A function.
  *
   @param x an argument
+
+  (keep)
  */
-// An implementation comment.
-/// A triple-slash comment.
+// An implementation comment. (DROP)
+/// A triple-slash comment. (DROP)
 export declare function f(
-  /** About parameter x */
+  /** About parameter x (keep) */
   x: string,
 );
